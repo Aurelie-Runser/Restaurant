208a087388c43de024ebb2f12cf02619d297c25b
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-10-03 23:37:17 +0200

    feat : myHeader
    création du composant "myHeader"

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+
+import myHeader from "@/components/myHeader.vue"
 </script>
 
 <template>
-    <header></header>
+    <myHeader/>
     <RouterView />
 </template>
--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,11 +1,33 @@
 <template>
+    <header class="header">
+        <a class="header__logo" href="/">
+            <img src="/Logo.png" alt="Logo de 'FOOD'">
+        </a>
 
+        <myHeaderMenu/>
+        <myHeaderSearch/>
+
+    </header>
 </template>
 
 <style lang="scss" scoped>
+.header{
+    position: sticky;
+    top: 0;
+    padding: 15px 0;
+    background: $color-white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
+    &__logo{
+        display: block;
+        width: 100px;
+    }
+}
 </style>
 
 <script setup>
-
+import myHeaderMenu from "@/components/elements/myHeaderMenu.vue"
+import myHeaderSearch from "@/components/elements/myHeaderSearch.vue"
 </script>
