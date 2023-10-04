d500ff66c7291ae02dd82fabdfbcbbdb89cc837e
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-10-04 16:07:11 +0200

    feat : myTitleSection
    création d'un composant pour les titres des sections

---
+++ src/components/elements/myTitleSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="title">
+    <h2>{{h2}}</h2>
+    <h3>{{h3}}</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.title{
+    margin: 57px auto;
+    text-align: center;
+    line-height: 1.5;
+
+    h2{
+        @include p2;
+        color: $color-main;
+    }
+}
+</style>
+
+<script setup>
+defineProps({
+  h2: String,
+  h3: String
+})
+</script>
