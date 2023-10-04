a37ccf2e64f897af9dd800c13667cc7c41341d65
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-10-04 23:29:15 +0200

    fix : myHeader z-index
    ajout d'un z-index

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,41 +1,41 @@
 <template>
   <header class="header">
     <div class="header__content">
-      <myLogo class="header__content__logo"/>
-      <myHeaderMenu/>
-      <myHeaderSearch/>
+      <myLogo class="header__content__logo" />
+      <myHeaderMenu />
+      <myHeaderSearch />
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
+.header {
+  z-index: 50;
+  position: sticky;
+  top: 0;
+  display: block;
+  width: 100vw;
+  margin-top: 15px;
+  background: $color-white;
 
-.header{
-    position: sticky;
-    top: 0;
-    display: block;
-    width: 100vw;
-    margin-top: 15px;
-    background: $color-white;
-    
-    &__content{
-        max-width: 85vw;
-        margin: auto;
-        padding: 15px 0;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    
-        &__logo{
-            display: block;
-            width: 100px;
-        }
+  &__content {
+    max-width: 85vw;
+    margin: auto;
+    padding: 15px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &__logo {
+      display: block;
+      width: 100px;
     }
+  }
 }
 </style>
 
 <script setup>
-import myLogo from "@/components/elements/myLogo.vue"
-import myHeaderMenu from "@/components/elements/myHeaderMenu.vue"
-import myHeaderSearch from "@/components/elements/myHeaderSearch.vue"
+import myLogo from '@/components/elements/myLogo.vue'
+import myHeaderMenu from '@/components/elements/myHeaderMenu.vue'
+import myHeaderSearch from '@/components/elements/myHeaderSearch.vue'
 </script>
