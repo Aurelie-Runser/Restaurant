dcfbf4080ca9a22820318826370281d85a92f08f
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-10-04 08:53:17 +0200

    feat : myFooter
    création du composant myFooter

---
+++ src/components/myFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer">
+
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer{
+    width: 100vw;
+    height: 200px;
+    background: black;
+}
+</style>
+
+<script setup>
+
+</script>
